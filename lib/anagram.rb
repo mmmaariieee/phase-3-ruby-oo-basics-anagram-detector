5609dafa123fc13b04cf0895eb1671ac3cdf4957
Mariia <m.goncharenkoo@icloud.com>
2022-10-31 20:01:02 -0400

    passed tests

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,10 @@
 # Your code goes here!
+class Anagram
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        array.filter {|text| text.chars.sort == @word.chars.sort}
+    end
+end
